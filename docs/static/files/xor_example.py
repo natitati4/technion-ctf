0ab6b5494acfc0a6e241f21bbb5b9f4449e8d228
nati <netanelkomm@gmail.com>
2025-05-03 01:05:01 +0300

    Delete docs, don't need it anymore

--- docs/static/files/xor_example.py
+++
@@ -1,6 +0,0 @@
-plaintext = input("Enter plaintext: ")
-integer = int(input("Enter integer: "))
-
-print("Your ciphertext is: ", end="")
-for char in plaintext:
-    print(chr(ord(char)^integer), end="")
